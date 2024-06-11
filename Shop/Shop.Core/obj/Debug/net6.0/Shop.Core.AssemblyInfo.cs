4ac62bb6b37e5b71bf27847ce9ed03494b7d1103
Bayramli Abbas <abbascb@code.local>
2024-06-11 14:18:18 +0400

    Not Done

--- Shop/Shop.Core/obj/Debug/net6.0/Shop.Core.AssemblyInfo.cs
+++ Shop/Shop.Core/obj/Debug/net6.0/Shop.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+842f7ff76af16293908da1b7ca5f1fef19fe6ee4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
